1056c567c120809365170ce9b8abd6bbb06be88c
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-17 12:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if(typeof(value1)===typeof(value2)){
 }
 	//your js code here
 
-
+}
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
