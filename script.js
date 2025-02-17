4964f97de42c4b4f51515516c6eb2e82984be3d8
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-17 13:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ if(!Number.isString(value2)){
 	value2 =Number(value2);
 }
 
-	if(Number.isString(value1)&&Number.isString(value2)){
+	if('string'(value1)&&'string'(value2)){
 return true;
 }
-	if(Number.isString(value1)||Number.isString(value2)){
+	if('string'(value1)||'string'(value2)){
 return false;
 }
 	
@@ -21,6 +21,6 @@ if(typeof(value1)===typeof(value2)){
 }
 	//your js code here
 // do not change the code below.
- let value1 = prompt("Enter Start of the Range.");
-  let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+//  let value1 = prompt("Enter Start of the Range.");
+//   let value2 = prompt("Enter End Of the Range.");
+// alert(isSameType(value1, value2));
