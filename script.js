d23fab691daf4a6c4e7b1d646efdbd9aaa77f0f9
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-17 13:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function isSameType(value1,value2){
-if(!Number.isInteger(value1)){
+if(!Number.isString(value1)){
 	value1 = Number(value1);
 }
-if(!Number.isInteger(value2)){
+if(!Number.isString(value2)){
 	value2 =Number(value2);
 }
 
-	if(Number.isInteger(value1)&&Number.isInteger(value2)){
+	if(Number.isString(value1)&&Number.isString(value2)){
 return true;
 }
-	if(Number.isInteger(value1)||Number.isInteger(value2)){
+	if(Number.isString(value1)||Number.isString(value2)){
 return false;
 }
 	
@@ -22,5 +22,5 @@ if(typeof(value1)===typeof(value2)){
 	//your js code here
 // do not change the code below.
 // let value1 = prompt("Enter Start of the Range.");
-//  let value2 = prompt("Enter End Of the Range.");
-// alert(isSameType(value1, value2));
+//   let value2 = prompt("Enter End Of the Range.");
+//  alert(isSameType(value1, value2));
