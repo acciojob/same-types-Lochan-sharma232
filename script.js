34cbd6f9fb7330dba2b3125f5fb3ca3a74c0c931
Lochan-sharma232 <lochansharma537@gmail.com>
2025-02-17 11:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function isSameType(value1,value2){
-if(Number.string("hey")&& Number.string("hello")){
+if(!isNaN(value1)){
+	value1=Number(value1);
+}
+if(!isNaN(value2)){
+	value2=Number(value2);
+}
+
+	if(!isNaN(value1)&&isNaN(value2)){
 return true;
-}else{
+}
+	if(!isNaN(value1)||isNaN(value2)){
 return false;
 }
-return typeof value1 === typeof value2;
+	
+if(typeof(value1)===typeof(value2)){
+	return false;
+}else{
+	return false;
+}
 }
 	//your js code here
 }
